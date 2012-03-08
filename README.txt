d25b0a3b7ecb251586158b6f505c451aa89bd53f
Patrick Suter <patrickasuter@gmail.com>
2012-03-08 07:07:06 +0100

    added rails skeleton

---
+++ README.txt
@@ -0,0 +1,14 @@
+##########################################
+
+Übersicht
+
+##########################################
+
+Diese Webapp erleichtert das abrufen des Schiedsrichtereinsatzes auf mobilen Endgeräten.
+Neben der normalen Ansicht, zeigt die Webapp auch erweiterte Informationen zum Einsatz an.
+
+##########################################
+
+Support
+
+##########################################
---
+++ sr_einsatz/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SrEinsatz::Application.config.secret_token = 'ca1853a0b2bdf01a0018e5ad0bcb33d356f3558f583d6b86c14d8f9781376f656779a7522795e4519664cc02b018908f8b4195febe597d1a830dfbb613c4dbe9'
